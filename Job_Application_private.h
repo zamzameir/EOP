e1434cde74948ab4109ca47e8d14faf7468290f4
zamzameir <zamzameir@gmail.com>
2016-04-16 02:09:29 +0800

    Stable codes

--- Job_Application_private.h
+++ Job_Application_private.h
@@ -5,19 +5,19 @@
 #define JOB_APPLICATION_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"2.0.1.0"
+#define VER_STRING	"2.0.1.1"
 #define VER_MAJOR	2
 #define VER_MINOR	0
 #define VER_RELEASE	1
-#define VER_BUILD	0
+#define VER_BUILD	1
 #define COMPANY_NAME	""
-#define FILE_VERSION	"2.0.1.0"
+#define FILE_VERSION	"2.0.1.1"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"2.0.1.0"
+#define PRODUCT_VERSION	"2.0.1.1"
 
 #endif /*JOB_APPLICATION_PRIVATE_H*/
